5da94ad7017836c275322a32d15f4c94146b45b3
lizainkognito2022-code <liza.inkognito2022@gmail.com>
2026-09-17 21:53:17 +0300

    Bot.py

---
+++ Bot.py
@@ -0,0 +1,23 @@
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": text
+    })
+
+try:
+    ip = requests.get(
+        "https://api.ipify.org",
+        timeout=10
+    ).text.strip()
+
+    send_message(f"🌐 IP GitHub Actions:\n{ip}")
+
+except Exception as e:
+    send_message(f"❌ Ошибка определения IP: {e}")
