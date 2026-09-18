88157ee60ace565b2a1d76789a40d9022db107c3
lizainkognito2022-code <liza.inkognito2022@gmail.com>
2026-09-18 05:51:56 +0300

    Bot.py

---
+++ Bot.py
@@ -0,0 +1,29 @@
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    requests.post(
+        url,
+        data={"chat_id": CHAT_ID, "text": text},
+        timeout=10
+    )
+
+try:
+    response = requests.get(
+        "https://api.bybit.com/v5/market/time",
+        headers={"User-Agent": "Mozilla/5.0"},
+        timeout=10
+    )
+
+    send_message(
+        f"🌐 Bybit:\n"
+        f"HTTP: {response.status_code}\n"
+        f"Ответ:\n{response.text[:3500]}"
+    )
+
+except Exception as e:
+    send_message(f"❌ Ошибка:\n{e}")
